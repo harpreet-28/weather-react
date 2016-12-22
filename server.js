3f8caf03e135e4eb23758bd251eb8c8b16455ff2
Harpreet Kaur <preet.k.k88@gmail.com>
2016-12-22 16:09:47 -0500

    removed bracked to solve problems

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var express = require('express');
 
 var app = express();
 const PORT = process.env.PORT || 9005;
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http']){
-next();
-  }else{
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
     res.redirect('http://' + req.hostname + req.url);
-  }
+    }
 });
 // we need to use Public Folder
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-  console.log(`Express Server is up on PORT: ${PORT}`);
+app.listen(PORT, function() {
+    console.log(`Express Server is up on PORT: ${PORT}`);
 });
