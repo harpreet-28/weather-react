e0cc622a1611e2906eca5f10f1a4e3881221db85
Harpreet Kaur <preet.k.k88@gmail.com>
2016-12-22 16:00:16 -0500

    redirecting to https to http

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var express = require('express');
 // create our app
 
 var app = express();
-var port = process.env.PORT || 9005;
+const PORT = process.env.PORT || 9005;
+app.use(function(req, res, next){
+  if(req.header['x-forwarded-proto'] === 'http']){
+next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 // we need to use Public Folder
 app.use(express.static('public'));
 
-app.listen(port, function(){
-  console.log(`Express Server is up on port: ${port}`);
+app.listen(PORT, function(){
+  console.log(`Express Server is up on PORT: ${PORT}`);
 });
